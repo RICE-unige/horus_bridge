474f080495c12eee06b0667b8b5caf5aa05219c9
Omotoye <adekoyaomotoye@gmail.com>
2024-03-19 18:35:18 +0100

    :construction: :bug: <not working> initialized the rust tcp bridge and the python to ros bridge, next fix all problems

--- horus_tcp_bridge/src/main.rs
+++ horus_tcp_bridge/src/main.rs
@@ -1,8 +1,13 @@
 // Import generated Protobuf code
-mod sensor_message;
 
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+mod sensor_message;
+mod multi_sensor_messages;
+
+use multi_sensor_messages::Message;
+use multi_sensor_messages::MessageType;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -29,12 +34,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut message_data = vec![0; message_len as usize];
             socket.read_exact(&mut message_data).await?;
 
-            // Deserialize the message using Protobuf
-            let message = sensor_message::Image::decode(&message_data[..])?;
+            // Deserialize the Message wrapper using Protobuf
+            let message = Message::decode(&message_data[..])?;
 
             // Handle the message (log, forward to bridge, etc.)
             println!("Received image: width: {}, height: {}, encoding: {}", message.width, message.height, message.encoding);
 
+            // Handle messages based on their type
+            // match message.type {
+            //     Some(Message::) => {
+            //         let image = message.image.unwrap(); 
+            //         println!("Received image: width: {}, height: {}, encoding: {}", image.width, image.height, image.encoding);
+            //     }
+            //     Some(MessageType::LASER_SCAN) => {
+            //         let laser_scan = message.laser_scan.unwrap(); 
+            //         println!("Received laser scan: ranges: {:?}", laser_scan.ranges);
+            //     }
+            //     Some(MessageType::IMU) => {
+            //         let imu = message.imu.unwrap();
+            //         println!("Received IMU data: orientation: {:?}", imu.orientation);
+            //     }
+            //     None => println!("Invalid message type"),
+            // }
             // ... (Potential bridge integration) ...
 
             socket.write_all("Hello from Rust!\n".as_bytes()).await?;
